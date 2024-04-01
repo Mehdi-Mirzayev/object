ed90b245fdea54d4eaa1fc5728512e37179dc850
Mehdi-Mirzayev <161413559+Mehdi-Mirzayev@users.noreply.github.com>
2024-04-01 06:59:37 +0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,61 @@
+//?   1.  Object Yarat.
+// let person = {
+//     name: "Mehdi", 
+//     age: 19,      
+//     city: "Nakhchivan"
+// };
+
+
+// console.log(person);
+
+//?   2.  Array icinde coxlu obyektler yaradin.
+
+// let people = [
+//     { name: "Hesen", age: 15, city: "Naxcivan" },
+//     { name: "Eli", age: 25, city: "Sumqayit" },
+//     { name: "Murad", age: 8, city: "Lenkeran" },
+//     { name: "Mehemmed", age: 12, city: "Baki" },
+//     { name: "Husen", age: 22, city: "Astara" }
+// ];
+
+
+// console.log(people);
+
+
+//?   3.  Telebeler obyektinde bir nece telebe elave edin. Hobbileri, xususiyettleri kimi icerisindede deyerleri qeyd edin.
+
+// let Telebeler = [
+//     {name: "Nihat", age: 25, hobbies: ["play pc games", "fitness", "chess"], xasiyeti: ["esebi","Mulayim"]},
+//     {name: "Fuad", age: 40, hobbies: ["fitness", "chess"], xasiyeti: ["Mulayim"]},
+//     {name: "Revan", age: 20, hobbies: ["chess"], xasiyeti: ["Sert"]}
+         
+
+// ];
+// console.log(Telebeler);
+
+//?   4.  Students arrayinin qiymet ortalamasini cixardin. (Butun telebelerin qiymetlerini alib sayini bolun.)
+
+
+
+//?   5.  Qiymeti 90-dan az olan telebeleri Ad ve Soyadi ile birlikde ekrana cixardib onlara xeberdarliq edin.
+
+// let sagirdler =[
+// {name:"Ayla", surname:"Aliyeva",qiymet:58},
+// {name:"Sema", surname:"Semedova",qiymet:98},
+// {name:"Nuray", surname:"Memmedova",qiymet:78}
+
+// ];
+// console.log(sagirdler);
+
+
+// function zeiftelebe(sagirdler) {
+//     console.log("Asagidaki telebelerin qiymeti 90-dan asagidir");
+//     for (let i = 0; i < sagirdler.length; i++) {
+//         if (sagirdler[i].qiymet<90) {
+//             console.log(sagirdler[i].name + " " + sagirdler[i].surname + " qiymet: " + sagirdler[i].qiymet);
+//             console.log("Diqqet! Qiymetiniz 90'dan azdi.");
+//         }
+        
+//     }
+// }
+// zeiftelebe(sagirdler);
